834e5beab0f2f11c21f160788375b26c34ef1562
Unnati-60 <94300638+Unnati-60@users.noreply.github.com>
2022-08-05 21:27:14 +0530

    Update professor.py

--- professor.py
+++ professor.py
@@ -1,10 +1,3 @@
-'''This is a 3 level Game, where player is asked to answer 10 math problems
-Player will get 3 chances to answer each problem correctly
-level 1 : contains One digits addition problems
-level 2 : contains Two digits additon problems
-level 3 : contains Three digits additon problems
-Score Out of 10 will be given to player at each level'''
-
 import random
 
 
